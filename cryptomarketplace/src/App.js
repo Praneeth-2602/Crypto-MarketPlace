5148d95f7f249d0e98538cd984007b83656b71da
lokesh-22 <lokeshboddupally13@gmail.com>
2024-03-20 11:50:22 +0530

    lokesh

--- cryptomarketplace/src/App.js
+++ cryptomarketplace/src/App.js
@@ -2,12 +2,22 @@
 import React from 'react';
 import Navbar from './components/navbar'; // Fix the casing of the file name
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />     
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
