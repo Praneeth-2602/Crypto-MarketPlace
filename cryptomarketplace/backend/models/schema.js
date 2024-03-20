e8e2c82c42a9c91f2c1f635df5095926bf5fca1c
Praneeth Palugula <praneeth.palugula@gmail.com>
2024-03-20 10:50:32 +0530

    praneeth

--- cryptomarketplace/backend/models/schema.js
+++ cryptomarketplace/backend/models/schema.js
@@ -1,1 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    name: String,location: String, email: String, password: String, role: String
+});
+module.exports = mongoose.model('User', userSchema);
