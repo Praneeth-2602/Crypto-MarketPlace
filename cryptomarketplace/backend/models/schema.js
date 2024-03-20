d3378f5ac3646a92fa21bdd2c18b4ac309f6b2d8
lokesh-22 <lokeshboddupally13@gmail.com>
2024-03-20 11:44:04 +0530

    lokesh

--- cryptomarketplace/backend/models/schema.js
+++ cryptomarketplace/backend/models/schema.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose
 
-const userSchema = new Schema({
-    name: String,location: String, email: String, password: String, role: String
-});
-module.exports = mongoose.model('User', userSchema);
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }   
+
+})
+
+module.exports = mongoose.model('user', UserSchema)
